4978f0175fd81ee0ad16ec93ad279bf51fc72368
jpmarin99 <60925818+jpmarin99@users.noreply.github.com>
2020-12-22 22:15:09 -0500

    Cambios de css y js

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Pablo\Documents\GitHub\EMSConalep\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Pablo\Documents\GitHub\EMSConalep\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\juanp\Documents\GitHub\EMSConalepWeb\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\juanp\Documents\GitHub\EMSConalepWeb\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
